315c6a15dd94f2f5374d52f755f456243200dc76
willyyyy32423 <williamtoro480@gmail.com>
2026-05-09 23:56:34 +0200

    Update traduci.js

--- api/traduci.js
+++ api/traduci.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 
     try {
         const { testo, linguaOrigine, linguaDestinazione } = req.body;
-        const API_KEY = process.env.GEMINI_API_KEY;
+        const API_KEY = "AIzaSyBew8ibyTclpJNvnWPZbhsZJmibGYtpiQQ";
 
         if (!API_KEY) {
             return res.status(500).json({ errore: "Manca GEMINI_API_KEY su Vercel" });
